999a2a48bd295b544f7c6764ceb5b8b7e1f922c7
Valeria <valeriamarsa13@gmail.com>
2022-09-26 00:09:36 -0600

    with lab 1 sol

---
+++ user/ps.c
@@ -0,0 +1,32 @@
+#include "kernel/param.h"
+#include "kernel/types.h"
+#include "kernel/pstat.h"
+#include "user/user.h"
+
+int
+main(int argc, char **argv)
+{
+  struct pstat uproc[NPROC];
+  int nprocs;
+  int i;
+  char *state;
+  static char *states[] = {
+    [SLEEPING]  "sleeping",
+    [RUNNABLE]  "runnable",
+    [RUNNING]   "running ",
+    [ZOMBIE]    "zombie  "
+  };
+
+  nprocs = getprocs(uproc);
+  if (nprocs < 0)
+    exit(-1);
+
+  printf("pid\tstate\t\tsize\tppid\tname\n");
+  for (i=0; i<nprocs; i++) {
+    state = states[uproc[i].state];
+    printf("%d\t%s\t%l\t%d\t%s\n", uproc[i].pid, state,
+                   uproc[i].size, uproc[i].ppid, uproc[i].name);
+  }
+
+  exit(0);
+}
