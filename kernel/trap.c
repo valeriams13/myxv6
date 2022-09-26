6459d9e2e7298139546aa661723defc5eb13b635
Valeria <valeriamarsa13@gmail.com>
2022-09-26 15:30:11 -0600

    added clockintr

--- kernel/trap.c
+++ kernel/trap.c
@@ -79,6 +79,7 @@ usertrap(void)
   // give up the CPU if this is a timer interrupt.
   if(which_dev == 2)
     yield();
+    clockintr();
 
   usertrapret();
 }
@@ -152,6 +153,7 @@ kerneltrap()
   // give up the CPU if this is a timer interrupt.
   if(which_dev == 2 && myproc() != 0 && myproc()->state == RUNNING)
     yield();
+    clockintr();
 
   // the yield() may have caused some traps to occur,
   // so restore trap registers for use by kernelvec.S's sepc instruction.
